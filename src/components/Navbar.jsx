2701e928a1836caef8ab3fe74eb6e2ecf8ed03f5
DevSatis <sksatishk9@gmail.com>
2023-03-05 19:20:10 +0530

    Navbar_Changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,82 @@
-import React from 'react';
+import React from "react";
 
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const Section = styled.div`
-    display: flex;
-    justify-content: center;
-`
+  display: flex;
+  justify-content: center;
+`;
+
 const Container = styled.div`
- 
+  width: 1200px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 0;
+`;
 
-`
 const Links = styled.div`
- 
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
 
-`
 const Icons = styled.div`
- 
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
 
-`
+const Logo = styled.img`
+  height: 50px;
+  background-color: transparent;
+`;
+
+const List = styled.ul`
+  display: flex;
+  gap: 20px;
+  list-style: none;
+`;
+
+const ListItem = styled.li`
+  cursor: pointer;
+`;
+
+const Icon = styled.img`
+  width: 20px;
+  cursor: pointer;
+`;
+
+const Button = styled.button`
+  width: 100px;
+  padding: 10px;
+  background-color: #da4ea2;
+  border-radius: 5px;
+  border: none;
+  color: white;
+  cursor: pointer;
+`;
 
 const Navbar = () => {
   return (
     <Section>
-        <Container >
-            <Links></Links>
-            <Icons></Icons>
-        </Container>
+      <Container>
+        <Links>
+          <Logo src="../public/img/logo.png" />
+          <List>
+            <ListItem>HOME</ListItem>
+            <ListItem>ABOUT US</ListItem>
+            <ListItem>WORKS</ListItem>
+            <ListItem>CONTACT</ListItem>
+          </List>
+        </Links>
+        <Icons>
+          <Icon src="./img/search.png" />
+          <Button>Hire Now</Button>
+        </Icons>
+      </Container>
     </Section>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
